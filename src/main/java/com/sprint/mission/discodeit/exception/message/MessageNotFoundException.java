9fb955206cd7766b431e5c123a87c8447e20f926
ChoiJiHye950 <principle950@daum.net>
2026-01-05 11:14:22 +0900

    ✨ Feat: MessageNotFoundException 클래스 추가 및 관련 코드 수정
    - Message 도메인에 대한 커스텀 예외 클래스(MessageNotFoundException) 추가
    - 기존 DiscodeitException에서 MessageNotFoundException으로 변경하여 가독성 및 유지보수성 개선

---
+++ src/main/java/com/sprint/mission/discodeit/exception/message/MessageNotFoundException.java
@@ -0,0 +1,16 @@
+package com.sprint.mission.discodeit.exception.message;
+
+import com.sprint.mission.discodeit.exception.ErrorCode;
+import java.util.Map;
+
+public class MessageNotFoundException extends MessageException {
+
+  public MessageNotFoundException(ErrorCode errorCode,
+      Map<String, Object> details) {
+    super(errorCode, details);
+  }
+
+  public MessageNotFoundException(ErrorCode errorCode) {
+    super(errorCode);
+  }
+}
--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
@@ -4,6 +4,7 @@ import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.exception.DiscodeitException;
 import com.sprint.mission.discodeit.exception.ErrorCode;
+import com.sprint.mission.discodeit.exception.message.MessageNotFoundException;
 import com.sprint.mission.discodeit.repository.MessageRepository;
 import com.sprint.mission.discodeit.service.MessageService;
 import java.util.List;
@@ -25,7 +26,7 @@ public class BasicMessageService implements MessageService {
   @Override
   public Message findById(UUID id) {
     return messageRepository.findById(id).orElseThrow(() ->
-        new DiscodeitException(ErrorCode.MESSAGE_NOT_FOUND));
+        new MessageNotFoundException(ErrorCode.MESSAGE_NOT_FOUND));
   }
 
   //채널 안의 메세지들을 모두 조회
