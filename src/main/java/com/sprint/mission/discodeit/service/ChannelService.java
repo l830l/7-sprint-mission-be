33cbb8de5d61ecc3cdf01e5f44e71f2ef8b28295
ChoiJiHye950 <principle950@daum.net>
2026-01-05 21:52:43 +0900

    🎨 Style: ChannelService 인터페이스 코드 포맷팅 수정
    - 들여쓰기 일관성 유지 및 가독성 향상

--- src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
@@ -3,28 +3,26 @@ package com.sprint.mission.discodeit.service;
 import com.sprint.mission.discodeit.dto.channel.request.ChannelCreateReq;
 import com.sprint.mission.discodeit.dto.channel.request.ChannelCreateSecReq;
 import com.sprint.mission.discodeit.dto.channel.request.ChannelUpdateReq;
-import com.sprint.mission.discodeit.dto.channel.response.ChannelPublicInfoRes;
 import com.sprint.mission.discodeit.entity.Channel;
-
 import com.sprint.mission.discodeit.entity.ChannelType;
+
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 public interface ChannelService {
 
-  Channel create(ChannelCreateReq req);
+    Channel create(ChannelCreateReq req);
 
-  Channel create(ChannelCreateSecReq req);
+    Channel create(ChannelCreateSecReq req);
 
-  Channel update(UUID id,
-      ChannelUpdateReq req);
+    Channel update(UUID id, ChannelUpdateReq req);
 
-  void delete(UUID id);
+    void delete(UUID id);
 
-  Map<ChannelType, List<Channel>> findAllByUserId(UUID userId);
+    Map<ChannelType, List<Channel>> findAllByUserId(UUID userId);
 
-  Channel findByName(String name);
+    Channel findByName(String name);
 
-  Channel findById(UUID id);
+    Channel findById(UUID id);
 }
