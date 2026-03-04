b63eb8b4c6e71e4397d01976aae76e7313958d16
ChoiJiHye950 <principle950@daum.net>
2026-03-04 14:20:15 +0900

    ✅ Test: UserDetailViewFacade 테스트 추가
    - 유저 id, 닉네임, 이메일로 조회하는 테스트 케이스 추가
    - UserDetailViewFacade 관련 로직 검증 및 Mock 객체 동작 확인

--- src/main/java/com/sprint/mission/discodeit/domain/user/facade/UserDetailViewFacade.java
+++ src/main/java/com/sprint/mission/discodeit/domain/user/facade/UserDetailViewFacade.java
@@ -1,48 +1,43 @@
 package com.sprint.mission.discodeit.domain.user.facade;
 
 import com.sprint.mission.discodeit.domain.binarycontent.dto.response.BinaryContentInfoRes;
+import com.sprint.mission.discodeit.domain.binarycontent.mapper.BinaryContentMapper;
 import com.sprint.mission.discodeit.domain.user.dto.response.UserDetailInfoRes;
 import com.sprint.mission.discodeit.domain.user.entity.User;
-import com.sprint.mission.discodeit.domain.userstatus.entity.UserStatus;
-import com.sprint.mission.discodeit.domain.binarycontent.mapper.BinaryContentMapper;
 import com.sprint.mission.discodeit.domain.user.mapper.UserMapper;
-import com.sprint.mission.discodeit.domain.binarycontent.service.BinaryContentService;
 import com.sprint.mission.discodeit.domain.user.service.UserService;
+import com.sprint.mission.discodeit.domain.userstatus.entity.UserStatus;
 import com.sprint.mission.discodeit.domain.userstatus.service.UserStatusService;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
-import org.springframework.transaction.annotation.Transactional;
-
 @Service
 @RequiredArgsConstructor
-
 public class UserDetailViewFacade {
 
     private final UserService userService;
-    private final BinaryContentService binaryContentService;
     private final UserStatusService userStatusService;
 
     //유저 단일 조회
     @Transactional(readOnly = true)
-    public UserDetailInfoRes findById(@NonNull UUID userId) {
+    public UserDetailInfoRes findById(UUID userId) {
         User user = userService.findById(userId);
         return toDetailInfo(user);
     }
 
     //유저 단일 조회 : 닉네임
     @Transactional(readOnly = true)
-    public UserDetailInfoRes findByNickname(@NonNull String nickname) {
+    public UserDetailInfoRes findByNickname(String nickname) {
         User user = userService.findByNickname(nickname);
         return toDetailInfo(user);
     }
 
     //유저 단일 조회 : 이메일
     @Transactional(readOnly = true)
-    public UserDetailInfoRes findByEmail(@NonNull String email) {
+    public UserDetailInfoRes findByEmail(String email) {
         User user = userService.findByEmail(email);
         return toDetailInfo(user);
     }
