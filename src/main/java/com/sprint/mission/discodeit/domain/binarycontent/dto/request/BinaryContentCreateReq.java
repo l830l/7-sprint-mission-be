a3c7e8695a10f43cbeea6cd6c7453f33547f141f
ChoiJiHye950 <principle950@daum.net>
2026-04-25 11:47:25 +0900

    ♻️ Refactor: BinaryContentCreateReq 필드 이름 변경 (`fileName` → `name`, `fileType` → `type`, `fileSize` → `size`)

--- src/main/java/com/sprint/mission/discodeit/domain/binarycontent/dto/request/BinaryContentCreateReq.java
+++ src/main/java/com/sprint/mission/discodeit/domain/binarycontent/dto/request/BinaryContentCreateReq.java
@@ -2,9 +2,9 @@ package com.sprint.mission.discodeit.domain.binarycontent.dto.request;
 
 public record BinaryContentCreateReq(
         byte[] data,
-        String fileName,
-        String fileType,
-        long fileSize
+        String name,
+        String type,
+        long size
 ) {
 
 }
