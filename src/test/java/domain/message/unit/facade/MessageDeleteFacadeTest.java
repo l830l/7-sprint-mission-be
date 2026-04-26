d5546c0b701d5eb8876b70ba37c068a5cddb19e2
ChoiJiHye950 <principle950@daum.net>
2026-04-26 16:36:49 +0900

    ♻️ Refactor: MessageDeleteFacade @NonNull 어노테이션 제거 및 테스트 코드 추가
    - deleteMessage 메서드에서 @NonNull 어노테이션 제거
    - MessageDeleteFacadeTest 단위 테스트 추가 (메세지 삭제 성공 케이스 검증)

---
+++ src/test/java/domain/message/unit/facade/MessageDeleteFacadeTest.java
@@ -0,0 +1,59 @@
+package domain.message.unit.facade;
+
+import com.sprint.mission.discodeit.domain.binarycontent.storage.BinaryContentStorage;
+import com.sprint.mission.discodeit.domain.channel.entity.Channel;
+import com.sprint.mission.discodeit.domain.message.entity.Message;
+import com.sprint.mission.discodeit.domain.message.facade.MessageDeleteFacade;
+import com.sprint.mission.discodeit.domain.message.service.MessageService;
+import domain.channel.fixture.ChannelFixture;
+import domain.message.fixture.MessageFixture;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+
+
+@ExtendWith(MockitoExtension.class)
+public class MessageDeleteFacadeTest {
+    @Mock
+    private MessageService messageService;
+
+    @Mock
+    private BinaryContentStorage binaryContentStorage;
+
+    @InjectMocks
+    private MessageDeleteFacade messageDeleteFacade;
+
+    @Nested
+    @DisplayName("메세지 삭제")
+    class DeleteMessage {
+        @Test
+        @DisplayName("성공: 유효한 messageId 가 들어오면 message가 삭제된다")
+        void success_delete_message() {
+            // given
+            Channel channel = ChannelFixture.createPublicChannel();
+            Message message = MessageFixture.create(channel.getId());
+
+            given(messageService.findById(message.getId())).willReturn(message);
+
+            // when
+            messageDeleteFacade.deleteMessage(message.getId());
+
+            // then
+            then(messageService).should(times(1)).findById(message.getId());
+            then(binaryContentStorage).should(times(message.getAttachments().size())).delete(any(UUID.class));
+            then(messageService).should(times(1)).delete(message.getId());
+
+        }
+    }
+}
