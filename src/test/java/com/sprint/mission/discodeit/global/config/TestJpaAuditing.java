551ba0f0e4a6f56cf87d96b30dc664f35dedb5c0
ChoiJiHye950 <principle950@daum.net>
2026-04-27 21:45:33 +0900

    ♻️ Refactor: JPA Auditing 설정 분리 및 테스트용 설정 클래스 추가
    - `@EnableJpaAuditing` 애플리케이션 클래스에서 제거 및 별도 설정 클래스로 분리
    - 테스트 환경용 `TestJpaAuditing` 및 `TestQueryDslConfig` 클래스 추가
    - 코드 가독성 및 관리 용이성 개선

---
+++ src/test/java/com/sprint/mission/discodeit/global/config/TestJpaAuditing.java
@@ -0,0 +1,7 @@
+package com.sprint.mission.discodeit.global.config;
+
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+public class TestJpaAuditing {
+}
